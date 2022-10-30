6c71f06e78755be36e26cb38815a2137b809cb03
BittingBarracuda <kmartinez2397@gmail.com>
2022-10-31 00:32:25 +0100

    Added first scraper model from Chapter 4.

---
+++ scraper_model1.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+from matplotlib.pyplot import title
+import requests
+
+class Content:
+    def __init__(self, topic, url, body):
+        self.topic = topic
+        self.title = title
+        self.body = body
+        self.url = url
+    
+    def print(self):
+        print(f'URL: {self.url}')
+        print(f'TITLE: {self.title}')
+        print(f'BODY:\n{self.body}')
+        print(f'URL: {self.url}')
